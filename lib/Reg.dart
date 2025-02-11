2046e3571f6ddf53b283fb18ecf53636e390697f
Moataz Shamsan <moataz7sha@gmail.com>
2025-02-12 00:22:05 +0300

    tsgil.dart

--- lib/Reg.dart
+++ lib/Reg.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_9/login.dart';
 
-class Register extends StatelessWidget {
-  const Register({super.key});
+class tsgil extends StatelessWidget {
+  const tsgil({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +13,7 @@ class Register extends StatelessWidget {
           leading: IconButton(onPressed: () {
             Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) =>const login()), (route) => false);
           }, icon: const Icon(Icons.arrow_back_outlined)),
-          title:const Text("Register",style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold , ),),
+          title:const Text("tsgil",style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold , ),),
           backgroundColor: const Color.fromARGB(255, 218, 103, 4),
         ),
         body: Padding(
@@ -23,7 +23,7 @@ class Register extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text("Register" , style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold),),
+                  const Text("tsgil" , style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold),),
                   const SizedBox(
                     height: 20.0,
                   ),
